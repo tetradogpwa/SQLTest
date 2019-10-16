648222fd6c1d9df4d2f2b30dd30fab7360f03bc1
gabriel <tetradog@gmail.com>
2019-10-16 10:26:30 +0200

    documentando un poco el código

--- utils.js
+++ utils.js
@@ -1,14 +1,13 @@
-//source:https://coderwall.com/p/flonoa/simple-string-format-in-javascript
-
 class StringUtils {
-
+    //source:https://coderwall.com/p/flonoa/simple-string-format-in-javascript
     static Format(str, ...args) {
-        args = ArrayUtils.Root(args);
-        for (var k = 0; k < args.length; k++) {
-            str = StringUtils.Replace(str, "{" + k + "}", args[k]);
+            args = ArrayUtils.Root(args);
+            for (var k = 0; k < args.length; k++) {
+                str = StringUtils.Replace(str, "{" + k + "}", args[k]);
+            }
+            return str;
         }
-        return str;
-    }
+        //source:https://coderwall.com/p/flonoa/simple-string-format-in-javascript
     static Replace(str, oldText, newText) {
         while (str.includes(oldText)) {
             str = str.replace(oldText, newText);
@@ -19,6 +18,7 @@ class StringUtils {
 }
 class ArrayUtils {
 
+    //sirve para obtener la array original de un ...args pasado por argumento a un metodo
     static Root(args) {
         while (args instanceof Array && args.length == 1 && args[0] instanceof Array)
             args = args[0];
