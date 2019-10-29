93e4a075596393256fdfdc36f1a248a216712d94
gabriel <tetradog@gmail.com>
2019-10-29 01:09:43 +0100

    commit

--- utils.js
+++ utils.js
@@ -223,7 +223,7 @@ class CacheUtils {
     }
 
     static GetByteArray(nombreCache, key) {
-        return CacheUtils.Get(nombreCache, key).then((result) => result.arrayBuffer());
+        return CacheUtils.Get(nombreCache, key).then((result) => result.blob().arrayBuffer());
 
     }
 
